37244856dd147fb6b3ea59086525a81022edd426
Dasaolu Daniel <yungdansa@gmail.com>
2025-09-18 06:27:16 +0100

    .

--- src/logic/modules/Notification.ts
+++ src/logic/modules/Notification.ts
@@ -2,10 +2,12 @@ import {
   MutationMarkNotificationsAsReadArgs,
   MutationSavePushNotificationTokenArgs,
   NotificationPaginator,
+  QueryGetNotificationsArgs,
 } from "../../gql/graphql"
 import { $api } from "../../services"
 import Common from "./Common"
-import { QueryGetNotificationsArgs } from "../../gql/graphql"
+import { PushNotifications } from "@capacitor/push-notifications"
+import { getPlatforms } from "@ionic/vue"
 
 export default class NotificationModule extends Common {
   constructor() {
@@ -13,6 +15,7 @@ export default class NotificationModule extends Common {
   }
 
   // Base Variables
+  public UnreadNotification = 0
   public ManyNotifications: NotificationPaginator | undefined
   public GetBitificationsPayload: QueryGetNotificationsArgs | undefined
 
@@ -25,11 +28,17 @@ export default class NotificationModule extends Common {
     this.ManyNotifications = undefined
   }
 
-  public GetNotifications = async (first: number, page: number) => {
-    return $api.notification.GetNotifications(first, page).then((response) => {
-      this.ManyNotifications = response.data?.GetNotifications
-      return this.ManyNotifications
-    })
+  public GetNotifications = async (
+    type: string,
+    first: number,
+    page: number
+  ) => {
+    return $api.notification
+      .GetNotifications(type, first, page)
+      .then((response) => {
+        this.ManyNotifications = response.data?.GetNotifications
+        return this.ManyNotifications
+      })
   }
 
   public SavePushNotificationDevice = () => {
@@ -42,15 +51,85 @@ export default class NotificationModule extends Common {
     }
   }
 
+  // public MarkNotificationsAsRead = async (
+  //   notificationIds: MutationMarkNotificationsAsReadArgs
+  // ) => {
+  //   return $api.notification
+  //     .MarkNotificationsAsRead(notificationIds)
+  //     .then(() => {
+  //       if (this.GetBitificationsPayload) {
+  //         this.GetNotifications(this.GetBitificationsPayload)
+  //       }
+  //     })
+  // }
+
   public MarkNotificationsAsRead = async (
     notificationIds: MutationMarkNotificationsAsReadArgs
   ) => {
     return $api.notification
       .MarkNotificationsAsRead(notificationIds)
       .then(() => {
-        if (this.GetBitificationsPayload) {
-          this.GetNotifications(this.GetBitificationsPayload)
-        }
+        this.GetNotifications("email", 1, 50)
       })
+  }
+
+  public addListeners = async () => {
+    await PushNotifications.removeAllListeners()
+
+    await PushNotifications.addListener("registration", (token) => {
+      this.PushNotificationDeviceForm = {
+        device_token: token.value,
+        device_type: getPlatforms()[0],
+      }
+      this.SavePushNotificationDevice()
+    })
+
+    await PushNotifications.addListener("registrationError", (_err) => {
+      // handle error here
+    })
+
+    await PushNotifications.addListener("pushNotificationReceived", () => {
+      // handle push notification
+      this.UnreadNotification++
+      // Logic.User.SaveUserActivity("Push Notification Received", "action");
+    })
+
+    await PushNotifications.addListener(
+      "pushNotificationActionPerformed",
+      (notification) => {
+        // handle notification click
+
+        const uuid = notification.notification.data.uuid
+
+        this.MarkNotificationsAsRead([uuid])
+
+        // Logic.User.SaveUserActivity("Push Notification Clicked", "action");
+      }
+    )
+  }
+
+  public registerNotifications = async () => {
+    // set unread notification container
+
+    if (localStorage.getItem("unread_notification") == null) {
+      localStorage.setItem("unread_notification", "0")
+    }
+
+    let permStatus = await PushNotifications.checkPermissions()
+
+    if (permStatus.receive === "prompt") {
+      permStatus = await PushNotifications.requestPermissions()
+    }
+
+    if (permStatus.receive !== "granted") {
+      console.error("User denied permissions!")
+    }
+
+    await PushNotifications.register()
+  }
+
+  public getDeliveredNotifications = async () => {
+    const notificationList = await PushNotifications.getDeliveredNotifications()
+    console.log("delivered notifications", notificationList)
   }
 }
